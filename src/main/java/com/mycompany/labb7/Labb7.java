cd9dae441c6d7dc18f65bf5d3363ba2ed266cc09
asmaa580 <asmaaadel918273@gmail.com>
2025-11-16 10:31:28 +0200

    main

---
+++ src/main/java/com/mycompany/labb7/Labb7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labb7;
+
+/**
+ *
+ * @author USER
+ */
+public class Labb7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
