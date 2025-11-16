0c5fd50667021a257b1f355a906844eff8dd3d54
habeeba148 <es.habiba.shady2023@alexu.edu.eg>
2025-11-16 11:45:34 +0200

    adding user class

---
+++ src/main/java/com/mycompany/labb7/User.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.labb7;
+import java.util.UUID;
+
+public abstract class User {
+    protected String userId;
+    protected String username;
+    protected String email;
+    protected String passwordHash;
+
+    public User(String username, String email, String passwordHash) {
+        this.userId = UUID.randomUUID().toString();
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public String getUserId() { return userId; }
+    public String getUsername() { return username; }
+    public String getEmail() { return email; }
+    public String getPasswordHash() { return passwordHash; }
+
+    public abstract String getRole(); 
+}
